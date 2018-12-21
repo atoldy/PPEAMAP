af4c3d20b0dc66303d3fada5e1b88e9b3bdbcd34
sio2 <sio2@a3316>
2018-12-21 14:05:31 +0100

    Changement du nom de la base de donnée bdberrue a bdamap

--- AMAP/util/connexion_sql.php
+++ AMAP/util/connexion_sql.php
@@ -1,7 +1,7 @@
 <?php
 // Connexion bdd
 try
-{$bdd = new PDO('mysql:host=localhost;dbname=bdberrue;charset=utf8','root','');}
+{$bdd = new PDO('mysql:host=localhost;dbname=bdamap;charset=utf8','root','');}
 
 catch(Exception $e)
 {die('Erreur : '.$e->getMessage());}
